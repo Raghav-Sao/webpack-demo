a7f4ae2129021c3d1142a7e20d3dfbe56fd90bef
Raghav <Raghav@Raghavs-MacBook-Pro.local>
2018-04-06 23:56:26 +0530

    extract text plugin to extract new css file

--- mywebpack.config.js
+++ mywebpack.config.js
@@ -1,10 +1,22 @@
 var UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+// const extractPlugin = new ExtractTextPlugin({
+// 	filename: 'main.css',
+// });
 var path = require('path');
 module.exports = {
 	entry: './src/js/app.js',
 	output: { path: path.resolve(__dirname, 'dist'), filename: 'bundle.js', publicPath: '/dist' },
 	module: {
-		rules: [{ test: /\.css$/, use: ['style-loader', 'css-loader'] }],
+		rules: [
+			{
+				test: /\.css$/,
+				use: ExtractTextPlugin.extract({
+					fallback: 'style-loader',
+					use: 'css-loader',
+				}),
+			},
+		],
 	},
-	plugins: [new UglifyJsPlugin()],
+	plugins: [new ExtractTextPlugin('main.css')],
 };
